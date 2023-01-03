7416f5f492e51c0cb87c0cb26c11b26052e8f350
Erik-Huguet <erik.huguet@le-campus-numerique.fr>
2023-01-03 13:01:45 +0100

    creation du contenu nomdelapage

---
+++ nomdelapage.php
@@ -0,0 +1,14 @@
+<!-- corps de la page -->
+<main class="container">
+    <div id="contenuPage">
+        <h1>Mon super site</h1>
+
+        <p>
+            Bienvenue sur mon super site !<br />
+            Vous allez adorer ici, c'est un site génial on y trouve de tout à petit prix... euh... Je cherche encore un peu le thème de mon site. :-D
+        </p>
+        <p>lorem ipsum dolor emet ....
+
+        </p>
+    </div>
+</main>
