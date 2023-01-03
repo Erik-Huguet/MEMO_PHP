19d98cc650935b22ad1e34897b6ec3cec5482b2a
Erik-Huguet <erik.huguet@le-campus-numerique.fr>
2023-01-03 13:02:25 +0100

    creation footer

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- pied de page -->
+<footer id="pied_de_page" class="container-fluid">
+    <div class="container-fluid p-2">
+        <p class="m-auto col-2">developped By John Doe</p>
+        <p class="m-auto col-2">Copyright moi, tous droits réservés</p>
+    </div>
+
+</footer>
